519e51c4e65aa018100b36791fe9a25d8de1b207
Bill Varcho <varcho@google.com>
2026-05-08 14:16:32 -0700

    [ReplicaGroupV3] Gate MeshAxesReplicaGroupList (RGV3) materialization in XLA SPMD partitioner with flag.
    PiperOrigin-RevId: 912691776

--- third_party/xla/xla/service/spmd/spmd_partitioner.h
+++ third_party/xla/xla/service/spmd/spmd_partitioner.h
@@ -338,6 +338,7 @@ class SpmdPartitioner : public HloModulePass {
 
   int64_t num_partitions() const { return num_partitions_; }
   int64_t num_replicas() const { return num_replicas_; }
+  bool enable_rgv3() const { return enable_rgv3_; }
 
  protected:
   absl::StatusOr<bool> RunImpl(
@@ -411,6 +412,7 @@ class SpmdPartitioner : public HloModulePass {
   SpmdPartitionerOptions options_;
   SPMDCollectiveOpsCreator collective_ops_creator_;
   absl::flat_hash_set<absl::string_view> execution_threads_;
+  bool enable_rgv3_ = false;
 };
 
 // Class describes partition state of the data represented by an HLO created
